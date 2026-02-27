9c77fa5d0f94c4e4f97f65fb236c676dca3eaa76
Colinxu2020 <63941938+colinxu2020@users.noreply.github.com>
2026-02-27 19:20:43 +0800

    :sparkles: Support Newer Huorong

--- scripts/merge_rules.py
+++ scripts/merge_rules.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: JerryLinLinLin
 # @Date:   2022-06-16 17:47:09
-# @Last Modified by:   JerryLinLinLin
-# @Last Modified time: 2022-06-27 17:48:32
+# @Last Modified by:   Colinxu2020
+# @Last Modified time: 2026-02-27 19:16:32
 
 '''
 Merge all rule.json into one file.
@@ -52,19 +52,20 @@ def main(input_path:str, output_path:str):
                 print("Merging file: %s" % auto_full_path)
                 # loop each auto in sub auto files
                 for each_key in dict(auto_dict["data"]).keys():
+                    if dict(auto_sum_dict["data"]).get(each_key) is None:
+                        auto_sum_dict["data"][each_key] = {}
+                        
                     # fix the blanks(v6)
                     for task in auto_dict["data"][each_key]:
                         task.setdefault("res_cmdline", "*")
                         task.setdefault("cmdline", '*')
                         task.setdefault("p_procname",'*')
                         task.setdefault("p_cmdline",'*')
-                    # check if key already exist
-                    if dict(auto_sum_dict["data"]).get(each_key) is None:
-                        # frist one
-                        auto_sum_dict["data"][each_key] = auto_dict["data"][each_key]
-                    else:
-                        auto_sum_dict["data"][each_key] = auto_sum_dict["data"][each_key] + \
-                            auto_dict["data"][each_key]  # add list to key
+
+                        if auto_sum_dict["data"][each_key].get(task["p_procname"]) is None:
+                            auto_sum_dict["data"][each_key][task["p_procname"]] = [task]
+                        else:
+                            auto_sum_dict["data"][each_key][task["p_procname"]].append(task)
 
     # dump json
     os.makedirs(output_path, exist_ok=True)
